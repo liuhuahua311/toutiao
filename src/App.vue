a1214d79e936ab81cd3f68c4592b5a28a03e814a
liuhuahua <1257508772@qq.com>
2019-08-22 21:43:58 +0800

    配置element-ui和配置vscode的eslint插件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    app<el-button type="success" disabled>成功按钮</el-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
