1380ac94f75d45c162f07bf9dd56c88ba2936273
liuhuahua <1257508772@qq.com>
2019-08-24 18:44:08 +0800

    完成表单校验和基础登录

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from '@/router'
+
+// 简单配置axios,在任何实例中都能使用到$http
+import axios from 'axios'
+Vue.prototype.$http = axios
 // @代表的是vue-cil提供的路径别名，src绝对路径的别名
 // 然后router下面的index.js省略了，在目录下默认索引文件（默认会去加载文件index名称的）
 // import Style from '@/style/index.less'
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class = 'container'>
+    <div class = 'container' ref='box'><!--添加ref属性     在页面渲染完毕后才拿到dom对象-->
         <el-card class="my-card">
             <img src="../../assets/images/logo_index.png" alt="">
-            <el-form :model="loginForm" >
-                <el-form-item>
+
+            <el-form ref="loginForm" :model="loginForm"  :rules="loginRules" status-icon>
+
+                <el-form-item prop="mobile">
                     <el-input v-model="loginForm.mobile" placeholder="请输入手机号码"></el-input>
                 </el-form-item>
-                <el-form-item>
+
+                <el-form-item prop="code">
                     <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:238px;margin-right:10px"></el-input>
                      <el-button>发送验证码</el-button>
                 </el-form-item>
@@ -14,7 +17,7 @@
                     <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" style="width:100%">登录</el-button>
+                    <el-button @click='login()' type="primary" style="width:100%">登录</el-button>
                 </el-form-item>
             </el-form>
         </el-card>
@@ -25,14 +28,60 @@
 
 <script>
 export default {
+  // 这个生命周期就是等页面加载完毕了，然后使用ref就能拿到整个div dom元素
+  mounted () {
+    const box = this.$refs.box
+    // $refs的意思，是当前组件使用过ref属性的元素集合（对象）
+    // ref='value'，这个属性值value是一个对象，就是集合了所有的属性值
+    console.log(box)// 这获取的是dom元素
+    const loginForm = this.$refs.loginForm
+    console.log(loginForm)// 这获取的是vue组件实例对象，可以调用里面的方法
+  },
   data () {
+    const checkMobile = (rule, value, callback) => {
+      // 自定义校验逻辑
+      // 是否是合法的手机号：第一位只能是1，第二位是3-9，剩下9位随意
+      // 用正则
+      if (!/^1[3-9]\d{9}$/.test(value)) {
+        return callback(new Error('手机号不合法'))
+      }
+      callback()
+    }
     return {
+      // 表单数据对象
       loginForm: {
         mobile: '',
         code: ''
 
+      },
+      // 表单校验规则对象
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号码', trigger: 'blur' },
+          { validator: checkMobile, tirgger: 'change' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '验证码长度为6位数', tirgger: 'blur' }
+        ]
       }
     }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm)
+            .then(res => {
+              this.$router.push('/')
+            })
+            .catch(() => {
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
+    }
+
   }
 }
 </script>
