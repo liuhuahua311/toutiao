8fdc1ff8b1a2f0a3e93349412ba45bd9d19640fe
liuhuahua <1257508772@qq.com>
2019-08-25 11:09:25 +0800

    完成了欢迎界面

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login'
+import Home from '@/views/home'
+
+import Welcome from '@/views/welcome'
 
 Vue.use(VueRouter)
 
@@ -8,7 +11,12 @@ const router = new VueRouter({
   routes: [
     // name选项作用：当路径太长时，用name值可以跳转
     // 跳转方便一些，之前是用$router.push('/login') 现在可以用 $router.push({name:'login})
-    { path: '/login', name: ' login', component: Login }
+    { path: '/login', name: ' login', component: Login },
+    { path: '/',
+      component: Home,
+      children: [
+        { path: '/', name: 'welcome', component: Welcome }
+      ] }
   ]
 })
 
